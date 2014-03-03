65cb194c40d674deef3c0105f81f8fd46a6ef28f
Gonzalo <gmoreno@acid.cl>
2014-03-03 18:46:00 -0300

    upgrade gem

--- app/assets/javascripts/store/spree_chilean_shipping.js
+++ app/assets/javascripts/store/spree_chilean_shipping.js
@@ -3,17 +3,65 @@
     $("#checkout_form_delivery").submit(function(){
       set_address_citybox($("#citybox_locations").val());
     });
+
+    $("#use_citybox").change(function(){
+      if(this.checked){
+        set_address_dummy();
+      } else {
+        remove_address_dummy();
+      }
+    });
+
+    if ($("#use_citybox").checked){
+      set_address_dummy();
+    } else {
+      remove_address_dummy();
+    }
+
+    function remove_address_dummy(){
+      if ($("#order_bill_address_attributes_address1").val() == 'dummy_address1')
+        $("#order_bill_address_attributes_address1").val('');
+
+      if ($("#order_bill_address_attributes_city").val() == 'dummy_city'){
+        $("#order_bill_address_attributes_city").val('');
+      }
+              
+      $(".citybox_hidden").show();
+    }
+
+    function hide_address_form(address_type){
+      if($("#new_" + address_type + "_address").data('active') == 0){
+        $("#new_" + address_type + "_address").data('active', 1)
+        _hide_address_form(address_type);
+      }
+    }
+    
+    function show_address_form(obj, address_type){
+      if($(obj).data('active') == 1){
+        $(obj).data('active', 0)
+        $("#order_" + address_type.substring(0, 4) + "_address_id").val(null);
+        _show_address_form(address_type);
+      }
+    }
+
+    function set_address_citybox(address){
+      $("#order_ship_address_attributes_address2").val(address);
+    }
+
+    function set_address_dummy(){
+      $(".citybox_hidden").hide();
+      $("#order_bill_address_attributes_address1").val('dummy_address1');
+      $("#order_bill_address_attributes_city").val('dummy_city');
+      $("#order_bill_address_attributes_zipcode").val('8320000');
+    }
   });
 
-  function set_address_dummy(state_id){
-  	$("#order_bill_address_attributes_address1").val('dummy_address1');
-  	$("#order_bill_address_attributes_city").val('dummy_city');
-  	$("#order_bill_address_attributes_state_id").val(state_id);
-  	$("#order_bill_address_attributes_zipcode").val('8320000');
-  }
+  
 
-  function set_address_citybox(address){
-    $("#order_ship_address_attributes_address2").val(address);
-  }
+  
+
+  
+
+  
   
 })(jQuery);
--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -4,16 +4,16 @@ Spree::CheckoutController.class_eval do
 
   def before_payment
     if @order.checkout_steps.include? "delivery"
-      if @order.shipments.first.shipping_method.citybox?
-      	citybox_address = @order.shipping_address.address2.split('//')
-      	@order.shipping_address.address1 = citybox_address[1].lstrip.rstrip
-      	@order.shipping_address.city     = citybox_address[2].lstrip.rstrip
-      	@order.shipping_address.phone    = @order.shipping_address.phone
+      if @order.shipments.first.shipping_method.citybox? and @order.shipping_address.address2.presence
+        citybox_address = @order.shipping_address.address2.split('//')
+        @order.shipping_address.address1 = citybox_address[1].lstrip.rstrip
+        @order.shipping_address.city     = citybox_address[2].lstrip.rstrip
+        @order.shipping_address.phone    = @order.shipping_address.phone
         @order.billing_address.address1 = citybox_address[1].lstrip.rstrip
         @order.billing_address.city     = citybox_address[2].lstrip.rstrip
         @order.billing_address.phone    = @order.shipping_address.phone
-      	@order.billing_address = @order.shipping_address
-      	@order.save
+        @order.billing_address = @order.shipping_address
+        @order.save
       end 
 
       packages = @order.shipments.map { |s| s.to_package }
@@ -26,7 +26,8 @@ Spree::CheckoutController.class_eval do
 
   # Check if bill_address has dummy data (citybox indicator), if true, disable, hide, or remove chilexpress shipment
   def before_delivery
-    if @order.bill_address.address1 == 'dummy_address1' and @order.bill_address.city == 'dummy_city'
+    if @order.bill_address.address1 == 'dummy_address1' or @order.bill_address.city == 'dummy_city' or @order.shipments.first.shipping_method.citybox?
+      @order.bill_address.state = Spree::State.find_by_name('Metropolitana')
       @order.shipping_method_id = Spree::ShippingMethod.find_by_name('Citybox Santiago').id
       @order.save
       @disable_all_except_citybox = true
