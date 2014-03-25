ef2a6f7d992e0c3cda369b522d25f7a582bbb30f
Gonzalo <gmoreno@acid.cl>
2014-03-25 10:23:17 -0400

    accept mail when guest checkout

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Spree::Order.class_eval do
   has_one :citybox_order
 
@@ -55,64 +54,4 @@ Spree::Order.class_eval do
       :phone            => order.shipping_address.phone
       )
   end
-=======
-Spree::Order.class_eval do
-  has_one :citybox_order
-
-  state_machine do
-    after_transition :to => :delivery, :from => :address do |order|
-      if order.shipments.all? { |shipment| shipment.shipping_rates.count == 1 }
-        order.shipments.each do |shipment|
-          shipment.selected_shipping_rate_id = shipment.shipping_rates.first.id
-        end
-      end
-    end
-  end
-
-  def finalize!
-	  touch :completed_at
-
-	  # lock all adjustments (coupon promotions, etc.)
-	  adjustments.update_all state: 'closed'
-
-	  # update payment and shipment(s) states, and save
-	  updater.update_payment_state
-	  shipments.each do |shipment|
-	    shipment.update!(self)
-	    shipment.finalize!
-	  end
-
-	  updater.update_shipment_state
-	  save
-	  updater.run_hooks
-    if self.shipments.first.shipping_method.citybox?
-      insert_citybox(self)
-    end
-
-	  deliver_order_confirmation_email
-	end
-
-  def insert_citybox(order)
-    citybox = Spree::CityboxOrder.create(
-      :customer_name    => "#{order.shipping_address.firstname} #{order.shipping_address.lastname}",
-      :citybox_location => order.shipping_address.company.split("//").first.lstrip,
-      :city             => order.shipping_address.city,
-      :country          => order.shipping_address.country.name,
-      :product          => "21",
-      :size             => "P",
-      :package          => true,
-      :order            => order,
-      :content          => "Artículos de vestuario",
-      :total            => order.total.to_i,
-      :kilogram         => 0,
-      :volume           => 0,
-      :ship_address     => nil,
-      :ship_city        => nil,
-      :email            => order.user.email,
-      :phone            => order.shipping_address.phone
-      )
-    order.shipping_address.company = nil
-    order.save
-  end
->>>>>>> 29819f4496bd710847761ff8f173c8e7a0572271
 end
